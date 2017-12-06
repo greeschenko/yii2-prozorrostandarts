c4fd84b1fa9ae354a90e9930272bd34839e0a777
Oleksiy Hryshchenko <greeschenko@gmail.com>
2017-12-06 22:05:57 +0200

    up

--- Module.php
+++ Module.php
@@ -10,6 +10,7 @@ class Module extends \yii\base\Module
         'UNIT' => 'https://raw.githubusercontent.com/openprocurement/standards/master/unit_codes/recommended/uk.json',
         'CVA' => 'http://standards.openprocurement.org/classifiers/cav/uk.json',
         'CAV-PS' => 'http://standards.openprocurement.org/classifiers/cav_v2/uk.json',
+        'CPV' => 'http://standards.openprocurement.org/classifiers/cpv/uk.json',
     ];
 
     public function init()
